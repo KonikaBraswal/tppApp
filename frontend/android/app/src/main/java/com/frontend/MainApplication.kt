29590f2019785636601b4e1c8e3d041769bac439
Gulam Mujtava <mustafak009@gmail.com>
2024-01-27 09:31:04 -0800

    web app

--- frontend/android/app/src/main/java/com/frontend/MainApplication.kt
+++ frontend/android/app/src/main/java/com/frontend/MainApplication.kt
@@ -19,7 +19,8 @@ class MainApplication : Application(), ReactApplication {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+              // add(MyReactNativePackage())                                                  
+              add(new RNSslPinningPackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
