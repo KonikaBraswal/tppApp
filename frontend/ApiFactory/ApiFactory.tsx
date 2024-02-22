ce45acf0b0b138e6ea97fa0e8dbd1298bd64314d
1192913 <minalkrishna66@gmail.com>
2024-02-22 13:56:25 +0530

    hi

--- frontend/ApiFactory/ApiFactory.tsx
+++ frontend/ApiFactory/ApiFactory.tsx
@@ -1,3 +1,58 @@
+// // ApiFactory.ts
+// import axios from 'axios';
+// import config from '../configs/config.json';
+// import sandboxConfig from '../configs/Sandbox.json';
+// import {Linking} from 'react-native';
+// import {TextInput, Button, View, Text} from 'react-native';
+// import SanboxApiClient from './SanboxApiClient';
+// //import RestApiClient from "./RestApiClient"; // Import the missing RestApiClient
+
+// let permissions: string[] = [];
+
+// class ApiFactory {
+//   private baseUrl: string;
+//   private clientId: string;
+//   private clientSecret: string;
+//   private commonHeaders: string;
+
+//   constructor() {
+//     this.baseUrl = config.baseUrl;
+//     this.clientId = config.clientId;
+//     this.clientSecret = config.clientSecret;
+//     this.commonHeaders = config.contentType;
+//   }
+
+//   createApiClient(type: string) {
+//     switch (type) {
+//       case 'sandbox':
+//         return new SanboxApiClient(
+//           this.baseUrl,
+//           this.clientId,
+//           this.clientSecret,
+//           this.commonHeaders,
+//         );
+//       // Add other cases if needed
+//       case 'local':
+//         return new SanboxApiClient(
+//           this.baseUrl,
+//           this.clientId,
+//           this.clientSecret,
+//           this.commonHeaders, // Assuming RestApiClient uses commonHeaders, adjust accordingly
+//         );
+//       case 'sit':
+//         return new SanboxApiClient(
+//           this.baseUrl,
+//           this.clientId,
+//           this.clientSecret,
+//           this.commonHeaders, // Assuming RestApiClient uses commonHeaders, adjust accordingly
+//         );
+//       default:
+//         throw new Error(`Invalid API client type: ${type}`);
+//     }
+//   }
+// }
+
+// export default ApiFactory;
 // ApiFactory.ts
 import axios from 'axios';
 import config from '../configs/config.json';
@@ -5,6 +60,7 @@ import sandboxConfig from '../configs/Sandbox.json';
 import {Linking} from 'react-native';
 import {TextInput, Button, View, Text} from 'react-native';
 import SanboxApiClient from './SanboxApiClient';
+import SandBox from './SandBox';
 //import RestApiClient from "./RestApiClient"; // Import the missing RestApiClient
 
 let permissions: string[] = [];
@@ -25,7 +81,7 @@ class ApiFactory {
   createApiClient(type: string) {
     switch (type) {
       case 'sandbox':
-        return new SanboxApiClient(
+        return new SandBox(
           this.baseUrl,
           this.clientId,
           this.clientSecret,
