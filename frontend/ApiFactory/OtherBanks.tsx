b37bdff0f29d5f29c8cf25fd4e5912d7e046f278
1192757 <konikabraswal@gmail.com>
2024-06-05 09:41:43 +0530

    hsbc_1

---
+++ frontend/ApiFactory/OtherBanks.tsx
@@ -0,0 +1,13 @@
+class OtherBanks{
+    private scope: any;
+  
+    constructor(scope: any) {
+      this.scope = scope;
+      console.log("Other API Factory initialized with scope:", scope);
+    }
+  
+    // Define methods and functionalities for the HSBC Sandbox API client
+  }
+  
+  export default OtherBanks;
+  
